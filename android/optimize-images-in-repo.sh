5743296ea773754165a7219915372a2476eec3f3
Michael Comella <michael.l.comella@gmail.com>
2018-10-01 13:41:43 -0700

    Issue #19: Add optimize-images-in-repo.sh.
    Proof of concept: https://github.com/mozilla-mobile/firefox-tv/pull/1236
    
    The plan is that another script will call this script on multiple
    repositories so we can quickly optimize the images on a number of
    repositories.

---
+++ android/optimize-images-in-repo.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Pulls the latest changes from master, losslessly optimizes all images recursively in
+# the working directory, and pushes the changes on a new branch in a git fork.
+#
+# This script expects the working directory to be a git repository with the following:
+# - A remote called "upstream" pointed to the latest master
+# - A remote called "origin" pointed to the fork to create the pull request from
+#
+# On macOS, this script will launch the ImageOptim GUI application and close it
+# upon completion.
+
+GIT=$(which git)
+IMAGE_OPTIM=/Applications/ImageOptim.app/Contents/MacOS/ImageOptim
+
+# config
+GIT_REMOTE_FORK=origin
+GIT_REMOTE_UPSTREAM=upstream
+
+SCRIPT_NAME=$(basename "$0")
+UUID=$(uuidgen)
+
+# macOS only. We should support linux via `trimage` later.
+if [ ! -f $IMAGE_OPTIM ]; then
+    echo "Error: $IMAGE_OPTIM not found. Install from https://imageoptim.com/mac. Exiting..." 1>&2
+    exit 1
+fi
+
+# via https://unix.stackexchange.com/a/155077
+if output=$(git status --porcelain) && [ -n "$output" ]; then
+    echo "Error: uncommited git changes. Skipping $(basename $(pwd))..." 1>&2
+    exit 1
+fi
+
+# I don't know if these are porcelain commands but we're not parsing output so it should be fine.
+# Run --quiet on spammy, less helpful, commands.
+$GIT fetch --quiet $GIT_REMOTE_UPSTREAM &&
+        $GIT checkout --quiet $GIT_REMOTE_UPSTREAM/master && \
+        $GIT checkout -b ni-imageoptim-$UUID && \
+        $IMAGE_OPTIM . && \
+        $GIT commit --all --message="No issue: losslessly optimize image assets.
+
+This process was completed automatically by the android-automation-tools script $SCRIPT_NAME." && \
+        $GIT push --no-verify $GIT_REMOTE_FORK
