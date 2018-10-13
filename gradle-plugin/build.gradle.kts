b4827d7ac01eca8ee400a9f1f3e5373c608aab61
Michael Comella <michael.l.comella@gmail.com>
2018-10-13 12:34:06 -0700

    Issue #24: Add mozilla-gradle plugin skeleton.

---
+++ gradle-plugin/build.gradle.kts
@@ -0,0 +1,25 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.2.71"
+}
+
+group = "org.mozilla.apt"
+version = "0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compile(kotlin("stdlib-jdk8"))
+    compile(gradleApi())
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ gradle-plugin/src/main/kotlin/org/mozilla/apt/MozillaPlugin.kt
@@ -0,0 +1,14 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.apt
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class MozillaPlugin : Plugin<Project> {
+
+    override fun apply(project: Project) {
+    }
+}
